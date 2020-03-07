8aac52d0306a08af980f7021a61dcc56b8dce36b
wenzheng-Zhu <zwz_322@126.com>
2020-03-07 15:42:59 +0800

    增加chatroom channel 和一些js

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,13 @@
+class ChatroomController < ApplicationController
+
+
+  before_action :require_user
+
+  def index
+  	@message = Message.new
+  	@messages = Message.custom_display
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  root 'chatroom#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+  #get '/signup', 'users#new'
+
+  mount ActionCable.server, at: '/cable'
+  
 end
